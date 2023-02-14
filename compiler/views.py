8a70a17a1a6a076b8c2dbac905dea75ef24ab967
JorBernt <berntsen.jorgen@gmail.com>
2023-02-14 14:12:29 +0100

    Added support for compiling and running csharp

--- compiler/views.py
+++ compiler/views.py
@@ -3,8 +3,18 @@ import os.path,subprocess,json
 from subprocess import STDOUT,PIPE
 from django.views.decorators.csrf import csrf_exempt
 
-def compile_java(java_file):
-    subprocess.check_call(['javac', java_file])
+def compile_java(file):
+    subprocess.check_call(['javac', file])
+
+def compile_csharp(file):
+    print(file)
+    subprocess.check_call(['mcs' , '-out:solution.exe', file])
+
+def execute_csharp(file, stdin):
+    cmd = ['mono', file]
+    proc = subprocess.Popen(cmd, stdin=PIPE, stdout=PIPE, stderr=STDOUT)
+    stdout,stderr = proc.communicate(stdin.encode())
+    return stdout.decode().strip()
 
 def execute_java(java_file, stdin):
     java_class,ext = os.path.splitext(java_file)
@@ -13,26 +23,35 @@ def execute_java(java_file, stdin):
     stdout,stderr = proc.communicate(stdin.encode())
     return stdout.decode().strip()
 
-def write_file(data):
-    if (os.path.exists("Solution.java") == False):
-        f = open("Solution.java", "x")
+def write_file(filename, data):
+    if (os.path.exists(filename) == False):
+        f = open(filename, "x")
     else:
-        f = open("Solution.java", "w+")
+        f = open(filename, "w+")
     f.write(data)
     f.close()
+
+
 
 @csrf_exempt
 def index(request):
     data = json.loads(request.body)
-    print(data)
-    write_file(data["UserCode"])
-    compile_java('Solution.java')
+    lang = data["Language"]
+    if lang == "csharp":
+        write_file("solution.cs",data["UserCode"])
+        compile_csharp("solution.cs")
+    if lang == "java":
+        write_file("Solution.java",data["UserCode"])
+        compile_java('Solution.java')
     testcases = data["TestCases"]
     results=[]
     for testcase in testcases:
         input = testcase["Input"]
         output = testcase["Output"]
-        res=execute_java("Solution", input)
+        if lang == "java":
+            res=execute_java("Solution", input)
+        if lang == "csharp":
+            res=execute_csharp("solution.exe",input)
         results.append(res)
     ret = json.dumps(results)
     print(ret)
