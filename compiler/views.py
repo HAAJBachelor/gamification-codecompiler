1d05212a922ee070c80cd4705bac5fb42334da26
JorBernt <berntsen.jorgen@gmail.com>
2023-03-13 12:31:07 +0100

    Added support for Python

--- compiler/views.py
+++ compiler/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-import os.path,subprocess,json
+import os.path,subprocess,json, sys
 from subprocess import STDOUT,PIPE
 from django.views.decorators.csrf import csrf_exempt
 
@@ -37,6 +37,41 @@ def execute_csharp(file, stdin, foldername):
 def execute_java(java_file, stdin, foldername):
     java_class,ext = os.path.splitext(java_file)
     cmd = ['runuser', '-l', 'coder', '-c', 'timelimit -t3 -T1 java -classpath ' + foldername + '/ Solution']
+    proc = subprocess.Popen(cmd, stdin=PIPE, stdout=PIPE, stderr=STDOUT)
+    stdout,stderr = proc.communicate(stdin.encode())
+    global out
+    out = stdout.decode().strip()
+    return proc.returncode
+
+def execute_javascript(file, stdin, foldername):
+    write_file("input.txt", stdin, foldername)
+    path = foldername + "/" + file
+    cwd = os.getcwd()
+    os.chdir(foldername)
+    cmd = [ 'timelimit',  '-t3',  '-T1', 'node',  path]
+    proc = subprocess.Popen(cmd, stdin=PIPE, stdout=PIPE, stderr=STDOUT)
+    os.chdir(cwd)
+    stdout,stderr = proc.communicate()
+    global out
+    out = stdout.decode().strip()
+    return proc.returncode
+
+def execute_typescript(file, stdin, foldername):
+    write_file("input.txt", stdin, foldername)
+    path = foldername + "/" + file
+    cwd = os.getcwd()
+    os.chdir(foldername)
+    cmd = [ 'timelimit',  '-t3',  '-T1', 'npx ts-node',  path]
+    proc = subprocess.Popen(cmd, stdin=PIPE, stdout=PIPE, stderr=STDOUT)
+    os.chdir(cwd)
+    stdout,stderr = proc.communicate()
+    global out
+    out = stdout.decode().strip()
+    return proc.returncode
+
+def execute_python(file, stdin, foldername):
+    path = foldername + "/" + file
+    cmd = ['runuser', '-l', 'coder', '-c', 'timelimit -t3 -T1 python ' + path]
     proc = subprocess.Popen(cmd, stdin=PIPE, stdout=PIPE, stderr=STDOUT)
     stdout,stderr = proc.communicate(stdin.encode())
     global out
@@ -94,12 +129,18 @@ def index(request):
         if rc != 0:
             delete_folder(foldername)
             return generate_compiler_error()
-    if lang == "java":
+    elif lang == "java":
         write_file("Solution.java", data["UserCode"], foldername)
         rc = compile_java('Solution.java', foldername)
         if rc != 0:
             delete_folder(foldername)
             return generate_compiler_error()
+    elif lang == "javascript" or lang == "typescript" :
+        data["UserCode"] = "const readline = require('../readline.js')\n" + data["UserCode"]
+        filename = "Solution.js" if lang == "javascript" else "Solution.ts"
+        write_file(filename, data["UserCode"], foldername)
+    elif lang == "python":
+        write_file("Solution.py", data["UserCode"], foldername)
     testcases = data["TestCases"]
     results=[]
     for testcase in testcases:
@@ -118,10 +159,29 @@ def index(request):
                 res = generate_runtime_error()
             else:
                 res = generate_test_result()
-        results.append(res)
+        if lang == "javascript":
+            rc = execute_javascript("Solution.js", input, foldername)
+            if rc != 0:
+                res = generate_runtime_error()
+            else:
+                res = generate_test_result()
+        if lang == "typescript":
+            rc = execute_javascript("Solution.ts", input, foldername)
+            if rc != 0:
+                res = generate_runtime_error()
+            else:
+                res = generate_test_result()
+        if lang == "python":
+            rc = execute_python("Solution.py", input, foldername)
+            if rc != 0:
+                res = generate_runtime_error()
+            else:
+                res = generate_test_result()
+    results.append(res)
     ret = {
         'Error' : False,
         'Results' : results
         }
     delete_folder(foldername)
+    print(ret)
     return HttpResponse(json.dumps(ret))
